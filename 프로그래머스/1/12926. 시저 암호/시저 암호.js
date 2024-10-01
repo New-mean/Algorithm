3e1d335344a7c0c4746c572c9a96d7cba0a8a644
New-mean <114797773+New-mean@users.noreply.github.com>
2024-10-01 21:20:13 +0900

    [level 1] Title: 시저 암호, Time: 29.40 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,26 +1,18 @@
 function solution(s, n) {
-    var answer = '';
-    for(let i =0 ; i <s.length; i++){
-    if (s[i] === " ") {
-        answer += " ";
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lower = "abcdefghijklmnopqrstuvwxyz";
+    var answer= '';
+
+    for(var i =0; i <s.length; i++){
+        var text = s[i];
+        if(text == ' ') {
+            answer += ' '; 
+            continue;
+        }
+        var textArr = upper.includes(text) ? upper : lower;
+        var index = textArr.indexOf(text)+n;
+        if(index >= textArr.length) index -= textArr.length;
+        answer += textArr[index];
     }
-      else {
-          let charCode = s.charCodeAt(i);
-      // 원래 대문자였는데
-      if (charCode <= 90) {
-        charCode += n;
-        // 변경 후 아스키코드값이 90보다 크다면
-        if (charCode > 90) charCode -= 26;
-      }
-      // 원래 소문자였는데
-      else {
-        charCode += n;
-        // 변경 후 아스키코드값이 122보다 크다면
-        if (charCode > 122) charCode -= 26;
-      }
-      answer += String.fromCharCode(charCode);
-    }
-         
-    }
-     return answer;
-  }
+    return answer;
+}
