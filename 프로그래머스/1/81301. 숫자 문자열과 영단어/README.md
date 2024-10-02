5bbfcb826a51e4b823486b0e13d1668fedd56f4c
New-mean <114797773+New-mean@users.noreply.github.com>
2024-10-02 10:02:54 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.39 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.09 ms
+메모리: 33.4 MB, 시간: 0.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 23일 09:30:16
+2024년 10월 02일 10:02:51
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,11 +1,13 @@
 function solution(s) {
-    const nbr = ["zero", "one", "two", "three" , "four", "five", "six", "seven", "eight", "nine"]
-    let answer = s
-    
-    for(let i =0 ; i < nbr.length; i++){
-        let arr = answer.split(nbr[i])
-         answer = arr.join(i)
-    }
-   return Number(answer)
-
+    s = s.replace(/zero/gi, 0)
+    .replace(/one/gi, 1)
+    .replace(/two/gi, 2)
+    .replace(/three/gi, 3)
+    .replace(/four/gi, 4)
+    .replace(/five/gi, 5)
+    .replace(/six/gi, 6)
+    .replace(/seven/gi, 7)
+    .replace(/eight/gi, 8)
+    .replace(/nine/gi, 9)
+    return parseInt(s);
 }
